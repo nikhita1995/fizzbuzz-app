5402e1231823792d1e6b4f800ab394703e93e933
nikhita1995 <sontakke.nikita@gmail.com>
2024-03-28 20:27:49 +0530

    Established UI and Service connectivity using axios

--- client/src/Component/Dashboard.js
+++ client/src/Component/Dashboard.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    const api = axios.create({
+        baseURL: "http://localhost:5000",
+    })
+
+    const [numbers, setNumbers] = useState([])
+
+    const getNumbers = async () => {
+        const apiData = await api.get('/numbers')
+        setNumbers(apiData.data)
+        console.log(apiData.data)
+      }
+
+    return (
+        <div>Dashboard
+            <button disabled={numbers.length > 0} onClick={getNumbers}>Fetch Data</button>
+        </div>
+    )
 }
 
 export default Dashboard
--- server/server.py
+++ server/server.py
@@ -8,8 +8,14 @@ CORS(app)
 #Endpoint to fetch altered values of 1-100 numbers 
 @app.route("/numbers", methods=["GET"])
 def getFizzBuzz():
-    print("Fizz Buzz Api Running!")
-    return {"msg": "Fizz Buzz Api Running!"}
+    #using List comprehension with multiple if elif statements
+    #If a number is multiple of 3 return "Fizz"
+    #If a number is multiple of 5 return "Buzz"
+    #If a number is multiple of both 3 & 5 return "FizzBuzz"
+    return [{"num": i, "val": "FizzBuzz"} if i%3 == 0 and i%5 == 0 
+            else {"num": i, "val": "Fizz"} if i%3 == 0 
+            else {"num": i, "val": "Buzz"} if i%5 == 0 
+            else {"num": i, "val": str(i)} for i in range(1, 101)] 
 
 
 if __name__ == "__main__":
